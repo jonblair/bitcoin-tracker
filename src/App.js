5759dcf8bad57ffd29f513d7888bd1ef211ae4de
JB <jonblair10@hotmail.com>
2018-07-03 14:59:05 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const _api = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      myChartName: null,
+      myDisclaimer: null,
+      myUpdatedTime: null,
+      myRateUS: null,
+      myRateCodeUS: null,
+      myRateSymbolUS: null,
+      myRateDescriptionUS: null,
+      myRateGB: null,
+      myRateCodeGB: null,
+      myRateSymbolGB: null,
+      myRateDescriptionGB: null,
+      myRateEU: null,
+      myRateCodeEU: null,
+      myRateSymbolEU: null,
+      myRateDescriptionEU: null
+    };
+
+  }
+
+  componentDidMount() {
+    this.getBitcoinData();
+  }
+
+  getBitcoinData() {
+    const request = async (_api) => {
+      const response = await fetch(_api);
+      const json = await response.json();
+
+      this.setState({
+        myChartName: json.chartName,
+        myDisclaimer: json.disclaimer,
+        myUpdatedTime: json.time.updated,
+        myRateUS: json.bpi.USD.rate,
+        myRateCodeUS: json.bpi.USD.code,
+        myRateSymbolUS: json.bpi.USD.symbol,
+        myRateDescriptionUS: json.bpi.USD.description,
+        myRateGB: json.bpi.GBP.rate,
+        myRateCodeGB: json.bpi.GBP.code,
+        myRateSymbolGB: json.bpi.GBP.symbol,
+        myRateDescriptionGB: json.bpi.GBP.description,
+        myRateEU: json.bpi.EUR.rate,
+        myRateCodeEU: json.bpi.EUR.code,
+        myRateSymbolEU: json.bpi.EUR.symbol,
+        myRateDescriptionEU: json.bpi.EUR.description
+      })
+    }
+    
+    request(_api);
+   
+   }
+
+  render() {
+
+    return (
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h1 className="app-title">{this.state.myChartName}</h1>
+        </header>
+        <p className="app-intro app-body">
+        <p>
+          Updated: {this.state.myUpdatedTime}
+        </p>
+        <p>
+          Prices: <br/>
+          <table className="priceTable">
+            <tr>
+              <td>$ {this.state.myRateUS} {this.state.myRateCodeUS}</td>
+            </tr>
+            <tr>
+              <td>£ {this.state.myRateGB} {this.state.myRateCodeGB}</td>
+            </tr>
+            <tr>
+              <td>€ {this.state.myRateEU} {this.state.myRateCodeEU}</td>
+            </tr>
+          </table>
+        </p>
+        </p>
+        <p className="app-footer">
+          {this.state.myDisclaimer}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
